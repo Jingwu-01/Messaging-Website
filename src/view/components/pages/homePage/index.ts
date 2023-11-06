78310445ca7f82fe1cbfcceb2523cdddb8db0f94
Jingwu Wang <jingwu@wangjingwudembp.dyndns.rice.edu>
2023-11-06 14:27:45 -0600

    login further developed

--- src/view/components/pages/homePage/index.ts
+++ src/view/components/pages/homePage/index.ts
@@ -6,54 +6,13 @@ class LoginPage extends HTMLElement {
     this.attachShadow({ mode: "open" });
 
     if (this.shadowRoot) {
-      // This could also come from an HTML template
-      this.shadowRoot.innerHTML = `
-        <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
-  <style>
-    * {
-      background-color: #add8e6;
-      text-align: center;
-    }
-    div {
-      background-color: #ffffff;
-    }
-    div p {
-      background-color: #ffffff;
-    }
-
-    div form {
-      background-color: #ffffff;
-    }
-    button {
-      background-color: #ffffff;
-    }
-    button iconify-icon {
-      background-color: #ffffff;
-    }
-    input {
-      background-color: #d9d9d9;
-      border: 1px solid #333;
-    }
-  </style>
-  <h1>Messaging</h1>
-  <p>A Messaging App for All Your Groups</p>
-  <div>
-    <p>Enter your username to login:</p>
-    <form id="username-box">
-      <input type="text" id="text_field" name="text_field" />
-      <button type="submit">
-        <iconify-icon
-          icon="ic:baseline-login"
-          id="username-submit"
-        ></iconify-icon>
-      </button>
-    </form>
-  </div>
-        `;
-    } else {
-      throw new Error("shadowRoot does not exist");
-    }
-  }
+      let template = document.querySelector("#counter-template");
+      if (!(template instanceof HTMLTemplateElement)) {
+        throw new Error("login page")
+      } else {
+      this.shadowRoot.append(template.content.cloneNode(true));} 
+    } 
+  } 
 
   connectedCallback(): void {
     const form = this.shadowRoot?.querySelector("#username-box");
@@ -62,7 +21,7 @@ class LoginPage extends HTMLElement {
     }
     this.controller = new AbortController();
     const options = { signal: this.controller.signal };
-    form.addEventListener("sumbit", this.handleSubmit.bind(this), options);
+    form.addEventListener("submit", this.handleSubmit.bind(this), options);
   }
 
   disconnectedCallback(): void {
@@ -73,8 +32,6 @@ class LoginPage extends HTMLElement {
   handleSubmit(event: SubmitEvent) {
     event.preventDefault();
     const username = usernameInput.value;
-
-    
 }
-
+} 
 customElements.define("login-page", LoginPage);
