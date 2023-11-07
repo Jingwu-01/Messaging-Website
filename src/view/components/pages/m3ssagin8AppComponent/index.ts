0a70a49bb7654554bbe01b80b1f02cf22ab5475e
tobahhh1 <manningunger@gmail.com>
2023-11-07 15:25:47 -0600

    added documentation

--- src/view/components/pages/m3ssagin8AppComponent/index.ts
+++ src/view/components/pages/m3ssagin8AppComponent/index.ts
@@ -1,6 +1,8 @@
 // Root component for the application.
 // Handles routing to the correct page.
 export default class M3ssagin8AppComponent extends HTMLElement {
+  // Map of each URL hash to the component
+  // that should be rendered when that hash is displayed
   routes: { [key: string]: string } = {
     "#/home": "<home-page></home-page>",
     "#/chat": "<chat-page></chat-page>",
@@ -22,13 +24,17 @@ export default class M3ssagin8AppComponent extends HTMLElement {
   connectedCallback() {
     // When the URL changes,
     // change the page we are on.
+
+    // needs to be a hash because there are no listeners for just a URL change for
+    // some reason
     window.addEventListener("hashchange", (event: Event) => {
+      // get the component of the page referred to by the hash
       let page_component = this.routes[window.location.hash];
-      console.log(window.location.hash);
+      // If it doesn't exist, throw up a 404 error
       if (!page_component) {
         page_component = `<h2>404 not found</h2>`;
       }
-      console.log(page_component);
+      // Render the page referred to by the hash
       let app_element = this.shadowRoot?.querySelector("#app");
       if (app_element) {
         app_element.innerHTML = page_component;
