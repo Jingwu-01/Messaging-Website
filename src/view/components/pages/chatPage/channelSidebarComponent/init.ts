c4bc978ab868d6491bdef4793c7727829538ccbc
jasonhan3 <98669315+jasonhan3@users.noreply.github.com>
2023-11-09 15:27:38 -0600

    added baseline code for styling channels/posts

--- src/view/components/pages/chatPage/channelSidebarComponent/init.ts
+++ src/view/components/pages/chatPage/channelSidebarComponent/init.ts
@@ -5,11 +5,21 @@ export default function channelDisplayComponentInit() {
         "beforeend",
 `
 <template id="channel-display-template">
+    <style>
+    .selected-channel {
+        text-color: white;
+        background-color: #26667C;
+    }
+    </style>
     <ul id="channel-list">
     </ul>
 </template>
 `
     );
+    // an example channel element looks like:
+    // <li id="channel-select-{channel-name}">{channel-name}</li>
+    // and a selected channel should look like:
+    // <li id="channel-select-{channel-name}" class="selected-channel">{channel-name}</li>
 
     customElements.define("channel-sidebar-component", ChannelSidebar);
 }
