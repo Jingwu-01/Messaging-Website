0c7aceb3630e1e15b89b600b584abaa4bcfd11e5
jasonhan3 <98669315+jasonhan3@users.noreply.github.com>
2023-11-15 17:27:44 -0600

    attempted adding event handlers for various buttons

--- src/view/components/pages/chatPage/postEditorComponent/index.ts
+++ src/view/components/pages/chatPage/postEditorComponent/index.ts
@@ -1,4 +1,17 @@
+type StringFunction = () => string;
+
 export class PostEditor extends HTMLElement {
+
+    // TODO: can definitely add abortcontroller for event handlers and
+    // 'deregistering' the event handlers here.
+
+    private postOperations: HTMLElement;
+
+    // TODO: can we make this more generic?
+    private postInput: HTMLTextAreaElement;
+
+    private postSubmit: HTMLElement;
+
     constructor() {
         super();
 
@@ -18,6 +31,103 @@ export class PostEditor extends HTMLElement {
 
         this.shadowRoot.append(template.content.cloneNode(true));
 
+        let postOperations = this.shadowRoot.querySelector("#post-operations");
+        let postInput = this.shadowRoot.querySelector("#post-form");
+        let postSubmit = this.shadowRoot.querySelector("#post-submit");
+
+        if (!(postOperations instanceof HTMLElement)) {
+            throw Error("Could not find an element with the post-operations id");
+        }
+        if (!(postInput instanceof HTMLTextAreaElement)) {
+            throw Error("Could not find a text area element with the post-form id");
+        }
+
+        if (!(postSubmit instanceof HTMLElement)) {
+            throw Error("Could not find an element with the post-submit id");
+        }
+
+        this.postOperations = postOperations;
+        this.postInput = postInput;
+        this.postSubmit = postSubmit;
+
+    }
+
+    connectedCallback() {
+        let postOperationElements = this.postOperations.children;
+        for (let childEl of postOperationElements) {
+            let id = childEl.id;
+            let splitId = id.split("-");
+            let operationType = splitId[1];
+            let innerTextFunc: StringFunction;
+            let prefixFunc: StringFunction;
+            let suffixFunc: StringFunction;
+            switch (operationType) {
+                case "reaction": {
+                    innerTextFunc = () => {return splitId[0]};
+                    break;
+                }
+                case "text": {
+                    innerTextFunc = () => {
+                        let startCharIdx = this.postInput.selectionStart;
+                        let endCharIdx = this.postInput.selectionEnd;
+                        return this.postInput.value.substring(startCharIdx, endCharIdx);
+                    }
+                    break;
+                }
+                default: {
+                    throw Error(`post editor connected callback: expected id of post operation to be of the form <operation>-text or <operation>-reaction, but id is: ${id}`);
+                }
+            }
+            switch (splitId[0]) {
+                case "bold":
+                    prefixFunc = this.boldMarkdown;
+                    suffixFunc = this.boldMarkdown;
+                    break;
+                case "italicize":
+                    prefixFunc = this.italicsMarkdown;
+                    suffixFunc = this.italicsMarkdown;
+                    break;
+                case "link":
+                    prefixFunc = this.urlPrefixMarkdown;
+                    suffixFunc = this.urlSuffixMarkdown;
+                    break;
+                default:
+                    // assume it's a reaction; there's no error handling here.
+                    prefixFunc = this.reactionMarkdown;
+                    suffixFunc = this.reactionMarkdown;
+                    break;
+            }
+            childEl.addEventListener("click", (evt: MouseEvent) => {
+                this.applyTextFormatting(prefixFunc, suffixFunc, selectedValFunc);
+            })
+        }
+    }
+
+    applyTextFormatting(prefixFunc: StringFunction, suffixFunc: StringFunction, selectedValFunc: StringFunction) {
+        let startCharIdx = this.postInput.selectionStart;
+        let endCharIdx = this.postInput.selectionEnd;
+        this.postInput.value = this.postInput.value.substring(0, startCharIdx) + 
+        prefixFunc() + selectedValFunc() + suffixFunc() + this.postInput.value.substring(endCharIdx);
+    }
+
+    reactionMarkdown() {
+        return ":";
+    }
+
+    boldMarkdown() {
+        return "*";
+    }
+
+    italicsMarkdown() {
+        return "**";
+    }
+
+    urlPrefixMarkdown() {
+        return "[";
+    }
+
+    urlSuffixMarkdown() {
+        return "]()";
     }
 
 
