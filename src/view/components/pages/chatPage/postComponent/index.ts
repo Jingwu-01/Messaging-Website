ced1e892e9a13fc630312f1ce1285bfb8e5e2cd7
Jingwu Wang <jingwu@wangjingwudembp.dyndns.rice.edu>
2023-11-29 01:05:14 -0600

    bold working, smile is close

--- src/view/components/pages/chatPage/postComponent/index.ts
+++ src/view/components/pages/chatPage/postComponent/index.ts
@@ -29,7 +29,8 @@ export class PostComponent extends HTMLElement {
     this.shadowRoot.append(template.content.cloneNode(true));
     let postHeader = this.shadowRoot.querySelector("#post-header");
     let postBody = this.shadowRoot.querySelector("#post-body");
-    let smileReactionComponent = this.shadowRoot.querySelector('#smile-reaction');
+    let smileReactionComponent =
+      this.shadowRoot.querySelector("#smile-reaction");
 
     if (!(postHeader instanceof HTMLElement)) {
       throw Error("Could not find an element with the post-header class");
@@ -37,13 +38,13 @@ export class PostComponent extends HTMLElement {
     if (!(postBody instanceof HTMLElement)) {
       throw Error("Could not find an element with the post-body class");
     }
-    if (!(smileReactionComponent instanceof ReactionComponent)) {
-      throw Error("Could not find a ReactionComponent element with the #smile-reaction id");
-    }
+    // if (!(smileReactionComponent instanceof ReactionComponent)) {
+    //   throw Error("Could not find a ReactionComponent element with the #smile-reaction id");
+    // }
 
     this.postHeader = postHeader;
     this.postBody = postBody;
-    this.smileReactionComponent = smileReactionComponent;
+    // this.smileReactionComponent = smileReactionComponent;
   }
 
   connectedCallback() {
@@ -79,9 +80,9 @@ export class PostComponent extends HTMLElement {
   addPostContent(viewPost: ViewPost): void {
     // TODO: obviously can add more functionality here later as needed.
     this.postPath = viewPost.path;
-    let rawMsg = viewPost.msg
-    let newText = this.replaceReactionText(rawMsg)
-    this.postBody.innerText = newText;
+    console.log("Posting the msg: " + viewPost.msg);
+    // this.postBody.innerText = this.formatText(viewPost.msg)
+    this.appendFormattedText(viewPost.msg, this.postBody);
     let postUserText = this.postHeader.querySelector("#post-user-text");
     // TODO handle error better
     if (postUserText != null) {
@@ -107,57 +108,57 @@ export class PostComponent extends HTMLElement {
       postTimeLongEl.innerHTML = postTimeObj.toString();
     }
 
-    let smileCount, frownCount, likeCount, celebrateCount: number
+    // let smileCount, frownCount, likeCount, celebrateCount: number
 
-    if (!(viewPost.reactions.smile) == undefined) {
-      smileCount = viewPost.reactions.smile.length 
-    } else {
-      smileCount = 0; 
-    }
-    
-    if (viewPost.reactions.frown) {
-      frownCount = viewPost.reactions.frown.length 
-    } else {
-      frownCount = 0; 
-    }
+    // if (!(viewPost.reactions.smile) == undefined) {
+    //   smileCount = viewPost.reactions.smile.length
+    // } else {
+    //   smileCount = 0;
+    // }
 
-    if (viewPost.reactions.like) {
-      likeCount = viewPost.reactions.like.length 
-    } else {
-      likeCount = 0; 
-    }
+    // if (viewPost.reactions.frown) {
+    //   frownCount = viewPost.reactions.frown.length
+    // } else {
+    //   frownCount = 0;
+    // }
 
-    if (viewPost.reactions.celebrate) {
-      celebrateCount = viewPost.reactions.celebrate.length 
-    } else {
-      celebrateCount = 0; 
-    }
+    // if (viewPost.reactions.like) {
+    //   likeCount = viewPost.reactions.like.length
+    // } else {
+    //   likeCount = 0;
+    // }
 
-    const smileReaction = this.shadowRoot?.querySelector("reaction-component") 
-    console.log(smileReaction)
-    console.log(smileReaction instanceof HTMLElement);
-    if (!(smileReaction instanceof ReactionComponent)){
-      throw new Error ("smileReaction is not a ReactionComponent")
-    }
-    smileReaction.addReactionCount(smileCount)
+    // if (viewPost.reactions.celebrate) {
+    //   celebrateCount = viewPost.reactions.celebrate.length
+    // } else {
+    //   celebrateCount = 0;
+    // }
 
-    const frownReaction = this.shadowRoot?.querySelector("#frown-reaction")
-    if (!(frownReaction instanceof ReactionComponent)){
-      throw new Error ("frownReaction is not a ReactionComponent")
-    }
-    frownReaction.addReactionCount(frownCount)
+    // const smileReaction = this.shadowRoot?.querySelector("reaction-component")
+    // console.log(smileReaction)
+    // console.log(smileReaction instanceof HTMLElement);
+    // if (!(smileReaction instanceof ReactionComponent)){
+    //   throw new Error ("smileReaction is not a ReactionComponent")
+    // }
+    // smileReaction.addReactionCount(smileCount)
 
-    const likeReaction = this.shadowRoot?.querySelector("#like-reaction")
-    if (!(likeReaction instanceof ReactionComponent)){
-      throw new Error ("likeReaction is not a ReactionComponent")
-    }
-    likeReaction.addReactionCount(likeCount)
+    // const frownReaction = this.shadowRoot?.querySelector("#frown-reaction")
+    // if (!(frownReaction instanceof ReactionComponent)){
+    //   throw new Error ("frownReaction is not a ReactionComponent")
+    // }
+    // frownReaction.addReactionCount(frownCount)
 
-    const celebrateReaction = this.shadowRoot?.querySelector("#celebrate-reaction")
-    if (!(celebrateReaction instanceof ReactionComponent)){
-      throw new Error ("celebrateReaction is not a ReactionComponent")
-    }
-    celebrateReaction.addReactionCount(celebrateCount)
+    // const likeReaction = this.shadowRoot?.querySelector("#like-reaction")
+    // if (!(likeReaction instanceof ReactionComponent)){
+    //   throw new Error ("likeReaction is not a ReactionComponent")
+    // }
+    // likeReaction.addReactionCount(likeCount)
+
+    // const celebrateReaction = this.shadowRoot?.querySelector("#celebrate-reaction")
+    // if (!(celebrateReaction instanceof ReactionComponent)){
+    //   throw new Error ("celebrateReaction is not a ReactionComponent")
+    // }
+    // celebrateReaction.addReactionCount(celebrateCount)
   }
 
   // Adds childrenPosts as replies to this ViewPost.
@@ -189,17 +190,33 @@ export class PostComponent extends HTMLElement {
   // handle filtering unstyled HTML with ** and stuff to strong and em
   // tags as needed
 
-  replaceReactionText(text: string): string {
-    const replacements: {[key: string]: string} = {
-      ':smile:': '<iconify-icon icon="lucide:smile" class="reaction-icon"></iconify-icon>', 
-      ':frown:': '<iconify-icon icon="lucide:frown" class="reaction-icon"></iconify-icon>', 
-      ':like:': '<iconify-icon icon="mdi:like-outline" class="reaction-icon"></iconify-icon>', 
-      ':celebrate:': '<iconify-icon icon="mingcute:celebrate-line" class="reaction-icon"></iconify-icon>'
+  appendFormattedText(text: string, container: HTMLElement): void {
+    // Regular expressions for different markdown patterns
+    const patterns = {
+      bold: /\*\*(.*?)\*\*/g,
+      italic: /\*(.*?)\*/g,
+      link: /\[(.*?)\]\((.*?)\)/g,
+      icon: /:(.*?):/g,
     };
 
-    return text.replace(/:smile:|:frown:|:like:|:celebrate:/g, (match) => replacements[match]);
-}
-  
+    // Replace the markdown with corresponding HTML elements
+    text = text
+      .replace(patterns.bold, (_, b) => `<strong>${b}</strong>`)
+      .replace(patterns.italic, (_, i) => `<em>${i}</em>`)
+      .replace(patterns.link, (_, text, url) => `<a href="${url}">${text}</a>`)
+      .replace(
+        patterns.icon,
+        (_, iconName) => `<iconify-icon icon="${iconName}"></iconify-icon>`
+      )
+      .replace(/\n/g, "<br>");
+
+    // Split the text into paragraphs
+    text.split(/\n\n/).forEach((paragraphText) => {
+      const paragraph = document.createElement("p");
+      paragraph.innerHTML = paragraphText;
+      container.appendChild(paragraph);
+    });
+  }
 }
 
 export default PostComponent;
