946a1ed0a48ad27b971a3ae0bf0c5a1f13ffa4f7
Jingwu Wang <jingwu@wangjingwudembp.dyndns.rice.edu>
2023-12-04 10:53:46 -0600

    reactionButton focus style added

--- src/view/components/pieces/reactionComponent/init.ts
+++ src/view/components/pieces/reactionComponent/init.ts
@@ -29,6 +29,14 @@ export default function reactionComponentInit() {
       .reacted {
         background-color: #add8e6
       }
+      #reaction-button:focus-visible {
+        box-shadow: rgba(60, 60, 60, 0.6) 0 0 0 3px;
+        outline: none;
+      }
+      #reaction-button:focus:not(:focus-visible) {
+        box-shadow: none;
+        outline: none;
+      }
     </style>
     <div id="reaction-container">
       <button id="reaction-button" aria-label="smile reaction">
